0fc3281981adf20e1ee4b1536a28a4b35020c8b8
Sean Tu <risole123@hotmail.com>
2019-03-17 17:34:04 +1000

    Initial Commit - Some prefabs - Assets folder structure - Native VR Support - No Scenes yet (SampleScene is just a test)

---
+++ Assets/Scripts/Machines/script_metal_sheet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_metal_sheet : MonoBehaviour
+{
+    public bool initialState = false;
+    private bool finished;
+
+    private GameObject cutout;
+
+    public void Awake()
+    {
+        cutout = transform.Find("metal_sheet_dropoff").gameObject;
+        finished = initialState;
+        updateState();
+    }
+
+    public void setState(bool isFinished)
+    {
+        finished = isFinished;
+        updateState();
+    }
+
+    public void updateState()
+    {
+        cutout.SetActive(!finished);
+    }
+}
---
+++ Assets/Scripts/Machines/script_pedestal_block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_pedestal_block : MonoBehaviour
+{
+    public bool initialState = false;
+    private bool finished;
+
+    private GameObject cutout;
+
+    public void Awake()
+    {
+        cutout = transform.Find("metal dropoff").gameObject;
+        finished = initialState;
+        updateState();
+    }
+
+    public void setState(bool isFinished)
+    {
+        finished = isFinished;
+        updateState();
+    }
+
+    public void updateState()
+    {
+        cutout.SetActive(!finished);
+    }
+}
