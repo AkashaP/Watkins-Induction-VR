24b4fab503e70e4d42dbfb6c0549da846250c4bc
Sean Tu <risole123@hotmail.com>
2019-03-29 15:47:36 +1000

    Added new controls to scene

---
+++ Assets/CameraAnchor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Forces this gameobject to look at a camera
+ */
+public class CameraAnchor : MonoBehaviour {
+    public Camera cam;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(cam.transform.position, cam.transform.up);
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
---
+++ Assets/Scripts/Controls/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Rotates when looked at
+ */
+public class Rotator : MonoBehaviour {
+    public static float lookAtThreshold = 1f;
+
+    public Camera cam;// = Camera.main;
+    public GameObject target;
+    public Vector3 rotation;
+
+    // Use this for initialization
+    void Start () {
+        //cam = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 lookAt = cam.WorldToViewportPoint(gameObject.transform.position);
+        if (lookAt.x <= lookAtThreshold && lookAt.y <= lookAtThreshold)
+        {
+            target.transform.Rotate(rotation);
+        }
+	}
+}
