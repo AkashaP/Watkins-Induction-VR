cd48d1e2f22008f4cd135db7bc58e7ca96f3e785
Draika the Dragon <nihao123456ftw@gmail.com>
2019-04-17 13:56:59 +1000

    New indicator script
    Beginning to work on a new target indicator script

---
+++ Assets/Scripts/Controls/TargetIndicator2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* Version of TargetIndicator that places objective indicators
+* Spatially, rather than as an overlay
+*/
+public class TargetIndicator2 : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject indicator;
+	private Camera player;
+
+	// Use this for initialization
+	void Start () {
+		player = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 heading = target.position - player.gameObject.transform.position;
+		Vector3 distance = heading.magnitude;
+		//Vector3 direction = heading / distance;
+		
+	}
+}
